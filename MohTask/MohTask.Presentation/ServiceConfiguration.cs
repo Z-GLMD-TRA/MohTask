45569799033e9f56f390f8dd90444f8f0ad7aba9
Zahra-Golmohammadi <147046866+Zahra-Golmohammadi@users.noreply.github.com>
2025-03-15 01:44:55 +0330

    add a service configuration class for add all settings

---
+++ MohTask/MohTask.Presentation/ServiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MohTask.Infrastructure;
+using MohTask.Infrastructure.Users;
+using MohTask.Service.Users;
+
+namespace MohTask.Presentation
+{
+    /// <summary>
+    /// Handles Dependency Injection configuration.
+    /// </summary>
+    public static class ServiceConfiguration
+    {
+        public static ServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Register DbContext
+            string connectionString = "Data Source=.;Initial Catalog=MohTaskDB;TrustServerCertificate=True;Integrated Security=SSPI";
+            services.AddDbContext<MohTaskDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            // Register Repository, Services, and Handlers
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<AccountManagement>();
+            services.AddScoped<InputAndOutputOperations>();
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
