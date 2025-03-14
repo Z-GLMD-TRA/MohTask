1d7a19996ea86708b64b6b166482d7802b2904f4
Zahra-Golmohammadi <147046866+Zahra-Golmohammadi@users.noreply.github.com>
2025-03-15 01:05:41 +0330

    create DTO classes for User entity

---
+++ MohTask/MohTask.DataTransferObject/Request/UserCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohTask.DataTransferObject.Request
+{
+    /// <summary>
+    /// used to send user information to the domain layer.
+    /// </summary>
+    public class UserCommand
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MohTask/MohTask.DataTransferObject/Response/UserResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohTask.DataTransferObject.Response
+{
+    /// <summary>
+    /// used to send user information to the presentation layer.
+    /// </summary>
+    public class UserResult
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public bool Status { get; set; }
+    }
+}
