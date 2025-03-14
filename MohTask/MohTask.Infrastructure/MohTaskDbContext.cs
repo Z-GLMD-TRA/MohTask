70583f6c9f25684059705ef7197000a1f998a0ed
Zahra-Golmohammadi <147046866+Zahra-Golmohammadi@users.noreply.github.com>
2025-03-14 23:41:09 +0330

    create and set the Dbcontext and install EfCore packages

---
+++ MohTask/MohTask.Infrastructure/MohTaskDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MohTask.Infrastructure.Users;
+using MohTask.Model.Users;
+
+namespace MohTask.Infrastructure
+{
+    public class MohTaskDbContext : DbContext
+    {
+
+        public MohTaskDbContext()
+        {
+
+        }
+        public MohTaskDbContext(DbContextOptions<MohTaskDbContext> dbContext) : base(dbContext)
+        {
+
+        }
+        /// <summary>
+        /// user table in DB
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// Configures the database connection if not already configured.
+        /// </summary>
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=MohTastDB;TrustServerCertificate=True;Integrated Security=SSPI");
+            }
+
+        }
+        /// <summary>
+        /// load and add models properties and relations
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ConfigureUserModelBuilder();
+        }
+    }
+
+}
---
+++ MohTask/MohTask.Infrastructure/Users/ConfigureUserModelBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MohTask.Model.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MohTask.Infrastructure.Users
+{
+    public static class UserModelBuilderConfiguration
+    {
+        public static void ConfigureUserModelBuilder(this ModelBuilder builder)
+        {
+            // Configure primary key
+            builder.Entity<User>().HasKey(u => u.Id);
+
+            // Configure properties
+            builder.Entity<User>()
+               .Property(u => u.Username)
+                      .IsRequired()
+                      .HasMaxLength(100);
+
+            builder.Entity<User>()
+              .Property(u => u.Password)
+                      .IsRequired()
+                      .HasMaxLength(255);
+
+            builder.Entity<User>()
+                .Property(u => u.Status)
+                  .HasDefaultValue(true);
+
+        }
+    }
+
+}
