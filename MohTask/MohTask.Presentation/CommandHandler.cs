3baee9fa96826906088b5626827c09b7c325ab56
Zahra-Golmohammadi <147046866+Zahra-Golmohammadi@users.noreply.github.com>
2025-03-15 01:46:11 +0330

    create a class for handling the commands which user enter

---
+++ MohTask/MohTask.Presentation/CommandHandler.cs
@@ -0,0 +1,47 @@
+﻿
+namespace MohTask.Presentation
+{
+    public class CommandHandler
+    {
+        private readonly AccountManagement _accountManager;
+        private readonly InputAndOutputOperations _inputOutputOperations;
+
+        public CommandHandler(AccountManagement accountManager, InputAndOutputOperations inputOutputOperations)
+        {
+            _accountManager = accountManager;
+            _inputOutputOperations = inputOutputOperations;
+        }
+
+        public async Task HandleCommand(string input)
+        {
+            var args = input.Split(" --", StringSplitOptions.RemoveEmptyEntries);
+            var command = args[0].ToLower();
+            var parameters = _inputOutputOperations.ParseParameters(args);
+
+            switch (command)
+            {
+                case "register":
+                    await _accountManager.RegisterUser(parameters);
+                    break;
+                case "login":
+                    await _accountManager.LoginUser(parameters);
+                    break;
+                case "change":
+                    await _accountManager.ChangeStatus(parameters);
+                    break;
+                case "search":
+                    await _accountManager.SearchUsers(parameters);
+                    break;
+                case "changepassword":
+                    await _accountManager.ChangePassword(parameters);
+                    break;
+                case "logout":
+                    _accountManager.Logout();
+                    break;
+                default:
+                    _inputOutputOperations.PrintMessage("Invalid command. Try again.", ConsoleColor.Red);
+                    break;
+            }
+        }
+    }
+}
