e81d7a64cae347427410c9dea2c59902e2e114fb
Zahra-Golmohammadi <147046866+Zahra-Golmohammadi@users.noreply.github.com>
2025-03-14 23:35:32 +0330

    add user entity with properties in domain layer

---
+++ MohTask/MohTask.Model/Users/User.cs
@@ -0,0 +1,14 @@
+﻿namespace MohTask.Model.Users
+{
+    /// <summary>
+    /// the user entity properties
+    /// </summary>
+    public class User
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty ;
+        //true for available and false for not available user
+        public bool Status { get; set; } = true;
+    }
+}
